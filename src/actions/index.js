60347af76e617d388fdd117d1a6e5b7b402ceb7a
David Leuliette <dleuliette@gmail.com>
2018-12-05 11:45:38 +0000

    Combine reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,15 +5,15 @@ export const TOGGLE_MESSAGE_DETAIL = 'TOGGLE_MESSAGE_DETAIL'
 
 /*
 
-Actions are JS objects with at least a key called type and a value which is 
-the name of the action (they might have some other key and values). 
+Actions are JS objects with at least a key called type and a value which is
+the name of the action (they might have some other key and values).
 Each of the following functions  return an action. These functions are called
 action creators
 
 */
 
 export const receiveThread = (thread) => ({
-    type: '?',
+    type: 'RECEIVE_THREAD',
     thread
 })
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,27 @@
 import { combineReducers } from 'redux'
 import {
-    RECEIVE_THREAD,
-    TOGGLE_MESSAGE_DETAIL,
-    RECEIVE_MESSAGE
+  RECEIVE_THREAD,
+  TOGGLE_MESSAGE_DETAIL,
+  RECEIVE_MESSAGE
 } from '../actions'
 
 export function uiReducer(state = { isMessageDetailOpen: true }, action) {
-    switch (action.type) {
-        default:
-            return state
-    }
+  switch (action.type) {
+    default:
+      return state
+  }
 }
 
-export function threadReducer(state, action) {
-    switch (action.type) {
-
-    }
+export function threadReducer(state = null, action) {
+  switch (action.type) {
+    case 'RECEIVE_THREAD':
+      return action.thread
+    default:
+      return state
+  }
 }
 
 export default combineReducers({
-    ui: uiReducer,
-    thread: '?',
-    // combineReducers can combine as many reducers as you need
+  ui: uiReducer,
+  thread: threadReducer
 })
